7f5e300f73a325de17c67b0834ac585f268068a1
vitormanoelcunhadecarvalhocarv@gmail.com <>
2025-09-15 19:47:20 -0300

    inicializado o repositorio Escola

---
+++ src/main/java/com/mycompany/escola/Escola.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.escola;
+
+/**
+ *
+ * @author vitor.mccarvalho
+ */
+public class Escola {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
